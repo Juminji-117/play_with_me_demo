75287ad611c4a3c32e01187507935fe2b866b1a6
Ju, Minji <alswl0w0@gmail.com>
2022-09-18 13:50:18 +0900

    feat : EventRepositoryCustom, EventRepositoryImpl 추가

--- playwithme/src/main/java/com/idea5/playwithme/event/repository/EventRepository.java
+++ playwithme/src/main/java/com/idea5/playwithme/event/repository/EventRepository.java
@@ -1,23 +1,16 @@
 package com.idea5.playwithme.event.repository;
 
-import com.idea5.playwithme.article.domain.Article;
 import com.idea5.playwithme.event.domain.Event;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
-public interface EventRepository extends JpaRepository<Event, Long> {
+public interface EventRepository extends JpaRepository<Event, Long>, EventRepositoryCustom {
 
     List<Event> findAll();
     List<Event> findByCategoryId(Integer id);
